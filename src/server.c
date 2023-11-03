b891cc482d646db834afa5a366be182b53a349c0
chiutamarcel <chiuta.marcel@gmail.com>
2023-11-03 17:49:00 +0200

    Fixed source formatting

--- src/server.c
+++ src/server.c
@@ -7,67 +7,36 @@
 #include <fcntl.h>
 
 
-
 int main() {
-
     int sd, cd;
-
     char buf[1000] = "", fname[10];
-
     struct sockaddr_in ser;
 
-
-
     // Create a socket
-
     sd = socket(AF_INET, SOCK_STREAM, 0);
-
     if (sd < 0)
-
         printf("SOCKET NOT CREATED\n");
 
-
-
     bzero(&ser, sizeof(struct sockaddr_in));
-
     ser.sin_family = AF_INET;
-
     ser.sin_port = htons(1101);
-
     inet_aton("localhost", &ser.sin_addr);
 
-
-
     int b = bind(sd, (struct sockaddr *)&ser, sizeof(ser));
-
     printf("BIND VALUE: %d\n", b);
-
-
 
     listen(sd, 5);
 
-
-
     for (;;) {
-
         cd = accept(sd, NULL, NULL);
-
         int pid = fork();
 
-
-
         if (pid == 0) {
-
             printf("accept value %d\n", cd);
-
             read(cd, buf, 1000);
-
             int fd = open(buf, O_RDONLY);
-
             read(fd, buf, 1000);
-
             write(cd, buf, strlen(buf));
-
             printf("MESSAGE FROM CLIENT: %s\n", buf);
 
             close(cd);
@@ -75,8 +44,6 @@ int main() {
         }
 
     }
-
-
 
     close(sd);
 
